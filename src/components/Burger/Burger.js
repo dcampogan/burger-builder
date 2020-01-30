163629049e7ddd80d1bec651189e233a4568e867
Dwayne Campogan <dcampogan@gmail.com>
2020-01-30 13:02:07 -0800

    Add ingredients ddynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,28 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type ={igKey} />;
+    });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = 
+      <p>Please start adding ingredients</p>
+  }
+
+  console.log(transformedIngredients);
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" /> {/* need type because of prop-type test*/}
-      <BurgerIngredient type="cheese" /> {/* need type because of prop-type test*/}
-      <BurgerIngredient type="meat" /> {/* need type because of prop-type test*/}
-      <BurgerIngredient type="bread-bottom" /> {/* need type because of prop-type test*/}
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" /> 
     </div>
   );
 };
